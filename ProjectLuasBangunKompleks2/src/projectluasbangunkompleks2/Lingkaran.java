45576703bd58ac909c4841ce3eec8aa06008fec7
latifahsabilil <71626855+latifahsabilil@users.noreply.github.com>
2021-10-08 16:24:16 +0700

    Create Lingkaran.java

---
+++ ProjectLuasBangunKompleks2/src/projectluasbangunkompleks2/Lingkaran.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projectluasbangunkompleks2;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class Lingkaran {
+    // atribut
+    int jejari;
+    
+    // method
+    int hitungLuas(){
+        int luas = this.jejari * this.jejari * 22 / 7;
+        return luas;
+    }
+}
