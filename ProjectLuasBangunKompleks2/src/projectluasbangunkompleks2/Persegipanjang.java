e08dad5081185650b3f91736f29e24a9f6c01849
latifahsabilil <71626855+latifahsabilil@users.noreply.github.com>
2021-10-08 16:24:18 +0700

    Create Persegipanjang.java

---
+++ ProjectLuasBangunKompleks2/src/projectluasbangunkompleks2/Persegipanjang.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projectluasbangunkompleks2;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class Persegipanjang {
+    int sisi1;
+    int sisi2;
+    
+    // method
+    int hitungLuas(){
+        int luas = this.sisi1 * this.sisi2;
+        return luas;
+    }
+}
